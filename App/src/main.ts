b1a41f5e0c8eeb7ce37eaa39d3f242100a36c813
Arun G Raj <arungraj@arun.com>
2026-02-01 20:37:55 +0530

    fix: restore main.ts imports and enable hash routing

--- App/src/main.ts
+++ App/src/main.ts
@@ -1,13 +1,35 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { RouteReuseStrategy, provideRouter, withPreloading, PreloadAllModules, withHashLocation } from '@angular/router';
-// ...
-provideRouter(routes, withPreloading(PreloadAllModules), withHashLocation()),
-  provideHttpClient(),
-  importProvidersFrom(IonicStorageModule.forRoot({
-    driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage]
-  })),
-  // Firebase Providers
-  provideFirebaseApp(() => initializeApp(environment.firebase)),
-  provideAuth(() => getAuth()),
+import { IonicRouteStrategy, provideIonicAngular } from '@ionic/angular/standalone';
+
+import { routes } from './app/app.routes';
+import { AppComponent } from './app/app.component';
+
+import { importProvidersFrom } from '@angular/core';
+import { IonicStorageModule } from '@ionic/storage-angular';
+import { Drivers } from '@ionic/storage';
+
+import { provideHttpClient } from '@angular/common/http';
+
+import { fancyAnimation } from './app/animations/nav.animation';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { environment } from './environments/environment';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    provideIonicAngular({
+      navAnimation: fancyAnimation
+    }),
+    provideRouter(routes, withPreloading(PreloadAllModules), withHashLocation()),
+    provideHttpClient(),
+    importProvidersFrom(IonicStorageModule.forRoot({
+      driverOrder: [Drivers.IndexedDB, Drivers.LocalStorage]
+    })),
+    // Firebase Providers
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
   ],
 });
